e77d79d4c714057c73e3ab0fccfe9caf4dac17cd
ibaldo <ig.flytolive@gmail.com>
2019-09-03 15:32:06 -0300

    Login

---
+++ lands/Lands/Lands/App.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Lands.Views;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+[assembly: XamlCompilation(XamlCompilationOptions.Compile)]
+namespace Lands
+{
+    public partial class App : Application
+    {
+        #region Constructor
+
+        public App()
+        {
+            InitializeComponent();
+
+            this.MainPage = new NavigationPage(new LoginPage());
+        }
+
+        #endregion
+
+        #region Methods
+
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }        
+
+        #endregion
+    }
+}
