8edda8714811de1d9e15134afa1aee0ea67bbac0
ibaldo <ig.flytolive@gmail.com>
2019-09-05 10:14:23 -0300

    LoginViewModel

---
+++ lands/Lands/Lands/Infrastructure/InstanceLocator.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using Lands.ViewModels;
+namespace Lands.Infrastructure
+{
+    public class InstanceLocator
+    {
+        #region Properties
+
+        public MainViewModel Main { get; set; }
+
+
+
+        #endregion
+
+        #region Constructor
+
+        public InstanceLocator()
+        {
+            this.Main = new MainViewModel();
+        }   
+
+        #endregion
+    }
+}
